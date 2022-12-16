3d39d9b0fb7921684ac80a19ce093fec8c40a5f9
jamison <hack.by.jamison@gmail.com>
2022-12-16 19:43:10 +0530

    min of 5 num

--- Topic wise example/Array/min-of-5-num.c
+++ Topic wise example/Array/min-of-5-num.c
@@ -1,34 +1,33 @@
-//input
+// input
 #include <stdio.h>
 int main()
 {
-    int a[5],i,min=0;
-    printf("enter the number ");
-    
-    for(i=0;i<5;i++)
+    int a[5], i, min = 0;
+
+    for (i = 0; i < 5; i++)
     {
-        scanf("%d",&a[i]);      //take input from the user
+        printf("enter the number ");
+        scanf("%d", &a[i]); // take input from the user
     }
 
-    min = a[1];                 //define one elimet to find out the minimun value of array
-    
-    for(i=0;i<5;i++)
+    min = a[1]; // define one elimet to find out the minimun value of array
+
+    for (i = 0; i < 5; i++)
     {
-        if (min>a[i])
+        if (min > a[i])
         {
-            min  = a[i];        //find min value which i given in array 
+            min = a[i]; // find min value which i given in array
         }
-        
     }
-    printf("the maximum number is: %d",min);    //print the minimum value of a array
+    printf("the maximum number is: %d", min); // print the minimum value of a array
 }
 
-//output
+// output
 /*
 enter the number 5
-5
-2
-55
-1
-the maximum number is: 1         
+enter the number 4
+enter the number 8
+enter the number 62
+enter the number 1
+the maximum number is: 1   
 */
