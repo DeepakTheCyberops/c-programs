a04321ea0c963133a7c44af7f759fdc25ebaaa8f
jamison <hack.by.jamison@gmail.com>
2022-12-16 13:37:19 +0530

    add fibonacci serie

---
+++ Topic wise example/Array/fibonacci-for-5-num.c
@@ -0,0 +1,29 @@
+//wap to print a fibonacci series for 5 num 
+//input
+#include <stdio.h>
+int main()
+{
+    int sum,a[5],i;
+    /*decleare two index a[0] or a[1]*/
+    a[0] = 0, a[1] = 1;
+
+    printf("first two value is: \n%d \n%d",a[0],a[1]);
+    
+    /*use loop for remaning 3 value */
+    for(i=2;i<5;i++)
+    {
+        sum = a[0]+ a[1];
+        printf ("\n%d",sum);
+        a[0] = a[1];
+        a[1] = sum;
+    }
+}
+
+/*
+first two value is: 
+0 
+1
+1
+2
+3                                                                                                                                                  
+*/
