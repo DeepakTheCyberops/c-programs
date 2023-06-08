7a96c149b86c637675a532a26664aa21b5e35ce5
Jamison4you <hack.by.jamison@gmail.com>
2023-06-08 11:44:33 +0530

    dsa

--- DSA/Searching/binary_search.c
+++ DSA/Searching/binary_search.c
@@ -13,33 +13,31 @@ int take_array_value(int size)
     scanf("%d", &value_for_find);
 
     x = binary_search(array, size, value_for_find);
-    printf(" The location of value you want to find: %d", x);
+    (x == -1) ? printf(" element is not found: %d", x) : printf(" The location of value you want to find: %d", x);  //check value is present or not 
 }
 
 int binary_search(int a[], int size, int find)
 {
-    int lowerbound = 0, midle, size_for_wile;
+    int lowerbound = 0, midle;
 
-    size_for_wile = size;
-
-    while (lowerbound < size_for_wile)
+    while (lowerbound <= size)
     {
-        midle = lowerbound + (size_for_wile - lowerbound) / 2;
+        midle = lowerbound + (size - lowerbound) / 2;
 
         if (a[midle] == find)
         {
             return midle;
-            break;
         }
-        else if (a[midle] < find)
+        if (a[midle] < find)    //if value is greater then the middle value then make lower bound equal to middle + 1
         {
             lowerbound = midle + 1;
         }
-        else
+        else    //if value is less then to the find value, then decrease the middle value and make upper bound value is mid -1
         {
-            lowerbound = midle - 1;
+            size = midle - 1;
         }
     }
+    return -1;  //if value is not find its return -1 value to the function
 }
 
 int main()
@@ -55,14 +53,18 @@ int main()
 }
 
 // output
-/*Size of array is: 8
-Enter the vlaue of array[0] is: -965
-Enter the vlaue of array[1] is: -456
-Enter the vlaue of array[2] is: 0
-Enter the vlaue of array[3] is: 645
-Enter the vlaue of array[4] is: 666
-Enter the vlaue of array[5] is: 777
-Enter the vlaue of array[6] is: 888
-Enter the vlaue of array[7] is: 999
-Enter a vale to find the location: 888
-The location of value you want to find: 6*/
+/*Size of array is: 12
+Enter the vlaue of array[0] is: 1
+Enter the vlaue of array[1] is: 2
+Enter the vlaue of array[2] is: 3
+Enter the vlaue of array[3] is: 4
+Enter the vlaue of array[4] is: 5
+Enter the vlaue of array[5] is: 6
+Enter the vlaue of array[6] is: 7
+Enter the vlaue of array[7] is: 8
+Enter the vlaue of array[8] is: 9
+Enter the vlaue of array[9] is: 10
+Enter the vlaue of array[10] is: 11
+Enter the vlaue of array[11] is: 12
+Enter a vale to find the location: 1
+The location of value you want to find: 0*/
